285a96114f847c9e088ee710f98866d0fe10825f
Muneeth <muneethseelam@gmail.com>
2026-07-24 22:29:14 +0530

    Add PC module with clock and reset functionality

--- rtl/RV32I/FETCH/pc.sv
+++ rtl/RV32I/FETCH/pc.sv
@@ -1,1 +1,18 @@
+module pc(
+    input  logic        clk,
+    input  logic        rst_n,
+    input  logic        StallF,
+    input  logic [31:0] pc_nxt,
+    output logic [31:0] pc_cur
+);
 
+always_ff @(posedge clk or negedge rst_n)
+begin
+    if(!rst_n)
+        pc_cur <= 32'd0;
+
+    else if(!StallF)
+        pc_cur <= pc_nxt;
+end
+
+endmodule
